408b0c0914542e130a4514427d920a095cdce37b
Cardy Wei <cardylw@gmail.com>
2014-03-24 20:09:44 -0400

    Added Hw#9 v2

---
+++ 09-LinkedList3/Node.java
@@ -0,0 +1,28 @@
+public class Node{
+	private String data;
+	private Node next;
+
+	public Node(String s){
+		data=s;
+	}
+
+	public void setNext(Node n){
+		next=n;
+	}
+
+	public void setData(String s){
+		data=s;
+	}
+
+	public String getData(){
+		return data;
+	}
+
+	public Node getNext(){
+		return next;
+	}
+
+	public String toString(){
+		return data;
+	}
+}
